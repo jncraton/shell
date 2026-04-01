bc23cb9e458090662b6d0ac303052284a820de4f
Jon Craton <jncraton@gmail.com>
2026-04-01 11:34:47 -0400

    Fix bug in first character of strcmp

--- shell.c
+++ shell.c
@@ -9,8 +9,8 @@ int strcmp(const char *s1, const char *s2) {
   unsigned char c1, c2;
 
   do {
-    c1 = *(++s1);
-    c2 = *(++s2);
+    c1 = *(s1++);
+    c2 = *(s2++);
     if (c1 == '\0')
       return c1 - c2;
   } while (c1 == c2);
